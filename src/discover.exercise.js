d112f4739ad0677aecce8a07175911cd7da295b1
Whitney Bluzma <thepunkyone@gmail.com>
2024-04-11 14:08:31 +0300

    feat: exercise 03 - data fetching

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -1,36 +1,34 @@
 /** @jsx jsx */
+import React, {useEffect} from 'react'
 import {jsx} from '@emotion/core'
 
 import './bootstrap'
 import Tooltip from '@reach/tooltip'
-import {FaSearch} from 'react-icons/fa'
+import {FaSearch, FaTimes} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
+
+import * as colors from './styles/colors'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const [query, setQuery] = React.useState('')
+  const [hasBeenQueried, setHasBeenQueried] = React.useState(false)
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  const {data, error, run, isLoading, isSuccess, isError} = useAsync()
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  useEffect(() => {
+    if (!hasBeenQueried) return
+
+    run(client(`books?query=${encodeURIComponent(query)}`))
+  }, [query, run])
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+
+    setHasBeenQueried(true)
+    setQuery(event.target.elements.search.value)
   }
 
   return (
@@ -54,11 +52,22 @@ function DiscoverBooksScreen() {
                 background: 'transparent',
               }}
             >
-              {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
+              {isError ? (
+                <FaTimes aria-label="error" css={{color: colors.danger}} />
+              ) : null}
+              {isLoading ? <Spinner /> : null}
+              {!isError && !isLoading ? <FaSearch aria-label="search" /> : null}
             </button>
           </label>
         </Tooltip>
       </form>
+
+      {isError ? (
+        <div css={{color: colors.danger}}>
+          <p>There was an error:</p>
+          <pre>{error.message}</pre>
+        </div>
+      ) : null}
 
       {isSuccess ? (
         data?.books?.length ? (
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,11 +1,19 @@
 function client(endpoint, customConfig = {}) {
-  // 🐨 create the config you'll pass to window.fetch
-  //    make the method default to "GET"
-  // 💰 if you're confused by this, that's fine. Scroll down to the bottom
-  // and I've got some code there you can copy/paste.
-  // 🐨 call window.fetch(fullURL, config) then handle the json response
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
+  const fullUrl = `${process.env.REACT_APP_API_URL}/${endpoint}`
+
+  const config = {
+    method: 'GET',
+    ...customConfig,
+  }
+
+  return window.fetch(fullUrl, config).then(async response => {
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      return Promise.reject(data)
+    }
+  })
 }
 
 export {client}
