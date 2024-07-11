3933faccde868c1563f1315d396d50c7217e4309
Whitney Bluzma <thepunkyone@gmail.com>
2024-07-11 15:28:14 +0100

    feat: complete credit 3

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,3 +1,5 @@
+import * as auth from '../auth-provider'
+
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(endpoint, {
@@ -14,7 +16,15 @@ function client(endpoint, {
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      await auth.logout()
+
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
+
     const data = await response.json()
+
     if (response.ok) {
       return data
     } else {
