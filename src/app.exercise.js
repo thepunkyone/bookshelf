b78b977439ad595de87b109c31aea4a02b19c9db
Whitney Bluzma <thepunkyone@gmail.com>
2024-07-11 15:13:06 +0100

    feat: complete credit 2

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,9 +3,12 @@ import {jsx} from '@emotion/core'
 import * as React from 'react'
 import * as auth from 'auth-provider'
 import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
 
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from './styles/colors'
 
 const getUser = async () => {
   const token = await auth.getToken()
@@ -23,27 +26,58 @@ const getUser = async () => {
 
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData
+  } = useAsync()
 
 
   React.useEffect(() => {
-    getUser().then(u => setUser(u))
-  }, [])
+    run(getUser())
+  }, [run])
 
 
   const login = (form) => {
-    return auth.login(form).then(u => setUser(u))
+    return auth.login(form).then(u => setData(u))
   }
 
   const register = (form) => {
-    return auth.register(form).then(u => setUser(u))
+    return auth.register(form).then(u => setData(u))
   }
 
   const logout = () => {
-    return auth.logout().then(() => setUser(null))
+    return auth.logout().then(() => setData(null))
   }
 
-  if (user) {
+  if (isIdle || isLoading) {
+    return <FullPageSpinner/>
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess && user) {
     return <AuthenticatedApp user={user} logout={logout}/>
   }
 
